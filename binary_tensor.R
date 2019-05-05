85f975f4c4dfbf19529735a47b5e96b2376cd9eb
Wang lab <saplingwang@gmail.com>
2019-05-04 23:24:25 -0500

    Add files via upload
    update

--- binary_tensor.R
+++ binary_tensor.R
@@ -2,6 +2,7 @@ library(rTensor)
 library(MASS)
 library(Matrix)
 library(rootSolve)
+library(gtools)
 ######################### main function for low-rank tensor estimation from binary observations #####
 ### Currently the algorithm only handels order-3 binary tensors with logit link. 
 ### Input: Y -- input binary tensor.
@@ -9,12 +10,15 @@ library(rootSolve)
 ### alpha: upper bound for the infinity norm.
 ### sigma: scale parameter in the logit distribution. By default set to 1, which corresponds to standard logit link. 
 ### option = 1: alternating glm algorithm; option = 2: MM algorithm. In general, MM algorithm is faster for large-scale binary tensors. 
+### random.ini: random initilization (TRUE) or spectral initilization (FALSE)
+### const: whether or not impose infinity norm bound on the parameter estimations. if const=TRUE, then the argument alpha will be ignored
+### nrand: number of random initilization. If random.ini=FALSE, then this argment will be ignored.
 
 ### output: 
 ### A,B,C -- factor matrices along each of the three modes. Each factor matrix is of dimension d_k-by-r, where d_k is the dimension at mode k and r is the pre-specified rank
 ### para -- estimated canonical (real-valued) tensor from multilinear glm model. 
 #########################
-binary_fit=function(Y,r,alpha=10,sigma=1,option=2){
+binary_fit=function(Y,r,alpha=10,sigma=1,option=2,random.ini=FALSE,const=TRUE,nrand=5){
     Y0=Y
     missing=which(is.na(Y)==T)
     nonmissing=which(is.na(Y)==F)
@@ -22,21 +26,48 @@ binary_fit=function(Y,r,alpha=10,sigma=1,option=2){
     
     Z=2*Y-1
     W=alpha/sigma*Z
-    nrand=5
+    
+    if(random.ini==TRUE){
+        
+        A=normalize(matrix(rnorm(dim(Y)[1]*r,0,1),nrow=dim(Y)[1]))
+        B=normalize(matrix(rnorm(dim(Y)[2]*r,0,1),nrow=dim(Y)[2]))
+          
+        Predictor=KhatriRao(A,B)
+        C=unfold(Z,3)%*%ginv(t(as.matrix(Predictor)))
+        M=tensorize(A,B,C)
+        
+        if((max(abs(M))>alpha/sigma)&(const==TRUE)){
+            C=C/max(abs(M))*(alpha/sigma)
+        }
+        
+        start=list("A"=A,"B"=B,"C"=C)
+        
+        fit0=binary_fit_fix(Y0,r,alpha/sigma,start,option,const)
+        cost=fit0$cost
+        
+        est=tensorize(fit0$A,fit0$B,fit0$C)
+        return(list("A"=fit0$A,"B"=fit0$B,"C"=fit0$C*sigma,"cost"=fit0$cost,"para"=est))
+        
+    }
+    
     
     if(r<=min(dim(Y))){ ## if rank is less than the minimum dimension
-        ## spectral initilization
+        
+        ## spectral initilization from unfoldings
         A=as.matrix(svd(unfold(W,1))$u[,1:r])
         B=as.matrix(svd(unfold(W,2))$u[,1:r])
         Predictor=KhatriRao(A,B)
-        C=unfold(W,3)%*%ginv(t(as.matrix(Predictor)))
+        C=unfold(Z,3)%*%ginv(t(as.matrix(Predictor)))
         M=tensorize(A,B,C)
-        if(max(abs(M))>alpha/sigma){
+        
+      
+        if((max(abs(M))>alpha/sigma)&(const==TRUE)){
             C=C/max(abs(M))*(alpha/sigma)
         }
+        
         start=list("A"=A,"B"=B,"C"=C)
         ## binary tensor decomposition
-        fit0=binary_fit_fix(Y0,r,alpha/sigma,start,option)
+        fit0=binary_fit_fix(Y0,r,alpha/sigma,start,option,const)
         cost0=fit0$cost
         
         ## random initilization
@@ -44,11 +75,11 @@ binary_fit=function(Y,r,alpha=10,sigma=1,option=2){
             start=CP_decomp(W,r)
             start=list("A"=start$A,"B"=start$B,"C"=start$C)
             M=tensorize(start$A,start$B,start$C)
-            if(max(abs(M))>alpha/sigma){
+            if((max(abs(M))>alpha/sigma)&(const==TRUE)){
                 start$C=start$C/max(abs(M))*(alpha/sigma)
             }
             ## binary tensor decomposition
-            fit=binary_fit_fix(Y0,r,alpha/sigma,start,option)
+            fit=binary_fit_fix(Y0,r,alpha/sigma,start,option,const)
             cost=fit$cost
             if(max(cost)>max(cost0)){
                 fit0=fit
@@ -59,15 +90,15 @@ binary_fit=function(Y,r,alpha=10,sigma=1,option=2){
     }
     else{ ## rank exceeds the minimum dimension
         
-        ## spectral initilization
+        ## spectral initilization from real-valued tensor decomposition
         start=CP_decomp(W,r)
         start=list("A"=start$A,"B"=start$B,"C"=start$C)
         M=tensorize(start$A,start$B,start$C)
-        if(max(abs(M))>alpha/sigma){
+        if((max(abs(M))>alpha/sigma)&(const==TRUE)){
             start$C=start$C/max(abs(M))*(alpha/sigma)
         }
         ## binary tensor decomposition
-        fit0=binary_fit_fix(Y0,r,alpha/sigma,start,option)
+        fit0=binary_fit_fix(Y0,r,alpha/sigma,start,option,const)
         cost0=fit0$cost
         
         ## random initilization
@@ -75,9 +106,9 @@ binary_fit=function(Y,r,alpha=10,sigma=1,option=2){
             start=CP_decomp(W,r)
             start=list("A"=start$A,"B"=start$B,"C"=start$C)
             ## binary tensor decomposition
-            fit=binary_fit_fix(Y0,r,alpha/sigma,start)
+            fit=binary_fit_fix(Y0,r,alpha/sigma,start,const)
             cost=fit$cost
-            if(max(cost)>max(cost0)){
+            if((max(abs(M))>alpha/sigma)&(const==TRUE)){
                 fit0=fit
                 cost0=cost
             }
@@ -96,7 +127,7 @@ binary_fit=function(Y,r,alpha=10,sigma=1,option=2){
 
 ######### subroutine for binary tensor decomposition with initilization start. 
 ## Option=1 alternating glm; option=2 miminization majorization
-binary_fit_fix=function(Y,r,alpha,start,option=2){
+binary_fit_fix=function(Y,r,alpha,start,option=2,const=TRUE){
     missing=which(is.na(Y)==T)
     nonmissing=which(is.na(Y)==F)
     Y[missing]=mean(Y,na.rm=T)
@@ -117,7 +148,9 @@ binary_fit_fix=function(Y,r,alpha,start,option=2){
 
     d=dim(Y)
     nloop=0
-    while((abs((cost_new-cost)/cost)>=0.001)&(nloop<50)){
+    
+    while((abs((cost_new-cost)/cost)>=0.001)&(nloop<50)){ ## modified for simulation
+    ##while(nloop<14){
         nloop=nloop+1
         A0=A
         B0=B
@@ -127,20 +160,26 @@ binary_fit_fix=function(Y,r,alpha,start,option=2){
         if(option==1){ ## alternating glm
         Predictor=KhatriRao(C,B)
         for(n in 1:d[1]){
-            res=glm(unfold(Y,1)[n,]~-1+as.matrix(Predictor),family=binomial(link='logit'))
-            A[n,]=coef(res)
+            res=glm_modify(unfold(Y,1)[n,],as.matrix(Predictor),start=A[n,])
+            A[n,]=res
+            #res=glm(unfold(Y,1)[n,]~-1+as.matrix(Predictor),family=binomial(link='logit'))
+            #A[n,]=coef(res)
         }
         
         Predictor=KhatriRao(A,C)
         for(n in 1:d[2]){
-            res=glm(unfold(Y,2)[n,]~-1+as.matrix(Predictor),family=binomial(link='logit'))
-            B[n,]=coef(res)
+            res=glm_modify(unfold(Y,2)[n,],as.matrix(Predictor),start=B[n,])
+            B[n,]=res
+            #res=glm(unfold(Y,2)[n,]~-1+as.matrix(Predictor),family=binomial(link='logit'))
+            #B[n,]=coef(res)
         }
         
         Predictor=KhatriRao(A,B)
         for(n in 1:d[3]){
-            res=glm(unfold(Y,3)[n,]~-1+as.matrix(Predictor),family=binomial(link='logit'))
-            C[n,]=coef(res)
+            res=glm_modify(unfold(Y,3)[n,],as.matrix(Predictor),start=C[n,])
+            C[n,]=res
+            #res=glm(unfold(Y,3)[n,]~-1+as.matrix(Predictor),family=binomial(link='logit'))
+            #C[n,]=coef(res)
         }
         
         res=rescale(A,B,C)
@@ -174,8 +213,9 @@ binary_fit_fix=function(Y,r,alpha,start,option=2){
             return(list("M"=M,"A"=A,"B"=B,"C"=C,"cost"=cost_traj))
         }
         
-        if(max(abs(M))>alpha){ ## project into the feasible space
-            
+     
+        if((max(abs(M))>alpha)&(const==TRUE)){ ## project into the feasible space
+            print("project back to the feasible space")
             fun_upper=function(x)constraint_max(x,A,B,C,A0,B0,C0,alpha)
             fun_lower=function(x)constraint_min(x,A,B,C,A0,B0,C0,alpha)
             find_root=uniroot.all(fun_upper,c(-10^(-4),1))
@@ -188,8 +228,9 @@ binary_fit_fix=function(Y,r,alpha,start,option=2){
             
             bound=c(bound,min(upper,lower))
             if(min(upper,lower)>0){
-                temp=optimize(function(x)search_new(x,A,B,C,Z,A0,B0,C0,nonmissing),lower=0,upper=min(upper,lower),maximum=TRUE)
-                gamma=temp$maximum
+                temp=optimize(function(x)search_new(x,A,B,C,Z,A0,B0,C0,nonmissing),lower=-10^(-4),upper=min(upper,lower),maximum=TRUE)
+                if(temp$maximum<0) gamma=0
+                else gamma=temp$maximum
             }
             else gamma=0
             
@@ -205,6 +246,7 @@ binary_fit_fix=function(Y,r,alpha,start,option=2){
             
             cost_new=objective(Z[nonmissing],M[nonmissing])
         }
+        
         cost_traj=c(cost_traj,cost_new) 
     }
     
@@ -218,7 +260,27 @@ binary_fit_fix=function(Y,r,alpha,start,option=2){
 }
 
 
-
+glm_modify=function(y,x,start){
+    
+    ## initial coefficent
+    ini_loglik=sum(log(inv.logit((2*y-1)*(x%*%start))))
+    
+    ## Option 1: glm fittig with default initilization
+    fit1 = glm(y~-1+x,family=binomial(link="logit"),control = list(maxit = 50))
+    ##return(coef(fit1))
+    ## Option 2: glm with user specified initilization
+    fit2= glm(y~-1+x,family=binomial(link="logit"),control = list(maxit = 50),start=start)
+    
+    ## report the result whichever gives the highest likelihood
+    value1=logLik(fit1)
+    value2=logLik(fit2)
+    if(is.na(value1)) value1=-Inf
+    if(is.na(value2)) value2=-Inf
+    
+    if(max(value1,value2)<ini_loglik) return (start)
+    else if(value1>value2) return(coef(fit1))
+    else return(coef(fit2))
+}
 
 
 ##################### construct CP tensor using factor matrices X, Y, Z ###################################
@@ -267,6 +329,51 @@ unfold=function(tensor,mode=1){
     }
     
     return(unfold)
+}
+
+tensordensity=function(p1,p2,p3){
+    r=dim(p1)[2]
+    prop=array(1,dim=c(dim(p1)[1],dim(p2)[1],dim(p3)[1]))
+    for(i in 1:r){
+        prob0=1-tensorize(p1[,i],p2[,i],p3[,i])
+        prop=prob0*prop
+    }
+    return(1-prop)
+}
+randomtensor=function(E){
+    Y=E
+    d=dim(E)
+    for(i in 1:d[1]){
+        for(j in 1:d[2]){
+            for(k in 1:d[3]){
+                Y[i,j,k]=rbinom(1,1,E[i,j,k])   
+            }
+        }
+    }
+    return(Y)
+}
+
+
+mismatch=function(tensor1,tensor2){
+    tensor1[tensor1>=0.5]=1
+    tensor1[tensor1<0.5]=0
+    
+    tensor2[tensor2>=0.5]=1
+    tensor2[tensor2<0.5]=0
+    return(mean(abs(tensor1-tensor2)))    
+}
+
+BIC=function(Y,rankrange,alpha,sigma,option,random.ini=TRUE,const=FALSE,nrand=0){
+    BIC_list=logL=pe=NULL
+    d=dim(Y)
+    for (rank in rankrange){
+        print(sprintf("fitting binary tensor with rank %d",rank))
+        fit=binary_fit(Y,rank,alpha,sigma,option,random.ini,const,nrand)
+        logL=c(logL,-2*objective(2*Y-1,fit$para))
+        pe=c(pe,rank*(sum(d)-length(d)+1))
+        BIC_list=c(BIC_list,-2*objective(2*Y-1,fit$para)+(rank*(sum(d)-length(d)+1)*log10(prod(d))))
+    }
+    return(list("BIC"=BIC_list,"log"=logL,"df"=pe*log10(prod(d)),"rank"=rankrange[which.min(BIC_list)],"AIC"=rankrange[which.min(logL+pe*2)]))
 }
 
 ## classical CP decomposition
@@ -398,4 +505,27 @@ low_rank=function(d,rank,alpha,sigma=1){
 error_bound=function(tensor1,tensor2){
     error=sqrt(sum((tensor1-tensor2)^2))
     return(error)
+}
+
+block=function(d,nc,option){
+    mu=array(0,dim=rep(nc,3))
+    if(option=="add"){
+        x=runif(nc,-1,1)
+        y=runif(nc,-1,1)
+        z=runif(nc,-1,1)
+        mu=x%o%rep(1,nc)%o%rep(1,nc)+rep(1,nc)%o%y%o%rep(1,nc)+rep(1,nc)%o%rep(1,nc)%o%z
+        return(pnorm(mu,0,1))
+    }
+    if(option=="mul"){
+        x=runif(nc,-1,1)
+        y=runif(nc,-1,1)
+        z=runif(nc,-1,1)
+        mu=x%o%y%o%z
+        return(pnorm(mu,0,1))
+    }
+    if(option=="com"){
+        mu=array(runif(nc^3,-1,1),dim=rep(nc,3))
+        return(pnorm(mu,0,1))
+    }
+    
 }
--- demo.R
+++ demo.R
@@ -8,11 +8,21 @@ sigma=1
 ## simulate a binary tensor with a rank-3 signal tensor
 simulate=low_rank(c(d,d,d),rank,alpha,sigma) ## simulate a rank-3 signal tensor with infinity norm bound alpha. 
 ttensor=simulate$parameter ## extract the real-valued canonical tensor
-Y=ttensor+array(rnorm(d^3,0,sigma),dim=c(d,d,d)) ## add Gaussian noise and generate the corresponding binary tensor 
-Y=1*(Y>0)
 
+Y=array(0,dim=rep(d,3))
+for(i in 1:d){
+    for(j in 1:d){
+        for(k in 1:d){
+            Y[i,j,k]=rbinom(1,1,inv.logit(ttensor[i,j,k]))
+        }
+    }
+}
+### generate Bernoulli tensor from logistic model
+
+## fit the model with input data Y
 option=1
-fit=binary_fit(Y,rank,alpha,sigma,option) ## estimate the low-rank tensor from the binary observations. 
+BIC_select=BIC(Y,1:5,alpha,sigma,option,random.ini=FALSE,const=TRUE,nrand=3) ## select rank from BIC 
+fit=binary_fit(Y,BIC_select$rank,alpha,sigma,option,random.ini=FALSE,const=TRUE,nrand=3) ## estimate the low-rank tensor from the binary observations. 
 
 ######################### main function for low-rank tensor estimation from binary observations #####
 ### Currently the algorithm only handels order-3 binary tensors with logit link. 
@@ -20,13 +30,17 @@ fit=binary_fit(Y,rank,alpha,sigma,option) ## estimate the low-rank tensor from t
 ### Y -- input binary tensor.
 ### r -- the prespecified rank.
 ### alpha -- upper bound for the infinity norm.
-### sigma -- scale parameter in the logit distribution. By default set to 1, which corresponds to standard logit link. 
-### option = 1: alternating glm algorithm; option = 2: MM (minimization-majorization) algorithm. In general, MM algorithm is faster but less accurate...Preferable for large-scale binary tensors. 
+### sigma: scale parameter in the logit distribution. By default set to 1, which corresponds to standard logit link. 
+### option = 1: alternating glm algorithm; option = 2: MM algorithm. In general, MM algorithm is faster for large-scale binary tensors. 
+### random.ini: random initilization (TRUE) or spectral initilization (FALSE)
+### const: whether or not impose infinity norm bound on the parameter estimations. if const=TRUE, then the argument alpha will be ignored
+### nrand: number of random initilizations in additional to the spectral initilization
+
 
 ### output: 
 ### A,B,C -- factor matrix along each of the three modes. Each factor matrix is of dimension d_k-by-r, where d_k is the dimension at mode k and r is the pre-specified rank
 ### para -- estimated canonical (real-valued) tensor from low-rank binary tensor model. 
 #########################
-
+plot(fit$para,ttensor) ## assess the estimation accuracy for the continous-valued tensor parameter
+plot(inv.logit(fit$para),inv.logit(ttensor)) #assess the estimation accuracy for the Bernoulli probability 
 error=sqrt(sum(ttensor-fit$para)^2/(d^3))
-table(Y-(fit$para>0)) ## observed binary tensor Y and estimated binary tensor from low-rank model
